9c0955d361070d3a0280445431a3ae7bc20fdb6e
withLevi <zengyin.ssr@gmail.com>
2024-05-20 16:11:31 +0800

    Add router and add view pages.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '@/App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import { HashRouter } from "react-router-dom";
 
-// import "normalize.css"
-import "./assets/css/index.less"
+import "normalize.css";
+import "./assets/css/index.less";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="Loading....">
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
   </React.StrictMode>
 );
-

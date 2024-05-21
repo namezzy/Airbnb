c7066fd5d6fee469e889d079a70f2cbfde0a1e6b
withLevi <zengyin.ssr@gmail.com>
2024-05-21 15:58:05 +0800

    Add reduxjs/toolkits and using createSlice and reducer of initialState two different method.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "normalize.css";
 import "./assets/css/index.less";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Suspense fallback="Loading....">
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
     </Suspense>
   </React.StrictMode>
 );
---
+++ src/store/modules/home.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const homeSlice = createSlice({
+    name: 'home',
+    initialState: {
+        productList: []
+
+    },
+
+    reducers: {
+
+    }
+})
+
+export default homeSlice.reducer

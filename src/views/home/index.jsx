0f3eaf4be4845ab6fd71697aec6ae62f7b1b0ca2
withLevi <zengyin.ssr@gmail.com>
2024-05-22 16:26:03 +0800

    Del network test of viem home

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,42 +1,9 @@
-import React, { memo, useEffect, useState } from 'react'
-import leviRequest from '@/services'
+import React, { memo} from 'react'
 
 const Home = memo(() => {
-  // 定义状态
-  const [highScore, setHighScore] = useState({})
-
-
-  // 网络请求的代码
-  useEffect(() => {
-    leviRequest.get({ url: "/home/highscore" }).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-  }, [])
-
   return (
     <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      {
-        // highScore.list && <ul>
-        //   {
-        //     highScore.list.map((item, index) => {
-        //       return <li key={index}>{item.name}</li>
-        //     })
-        //   }
-        // </ul>
-
-
-        <ul>
-          {
-            // 使用可选列
-            highScore.list?.map((item, index) => {
-              return <li key={index}>{item.name}</li>
-            })
-          }
-        </ul>
-      }
+      <h2>Home Page</h2>
     </div>
   )
 })

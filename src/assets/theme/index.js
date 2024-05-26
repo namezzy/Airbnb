01fc10d1d94165278a6d764491fee2d2061131a9
withLevi <zengyin.ssr@gmail.com>
2024-05-26 13:58:13 +0800

    Add Header's right side layout and content display.

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -1,10 +1,12 @@
 const theme = {
-    color: {
-        primaryColor: "#ff385c",
-        secondaryColor: "#00848A"
-    }
-}
+  color: {
+    primaryColor: "#ff385c",
+    secondaryColor: "#00848A",
+  },
+  text: {
+    primaryColor: "#484848",
+    secondaryColor: "#222",
+  },
+};
 
-export default theme
-
-
+export default theme;

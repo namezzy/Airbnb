116a20fe89fc3f17da18d37351e7db5d629757a2
withLevi <zengyin.ssr@gmail.com>
2024-06-01 23:59:28 +0800

    Add room item.

---
+++ src/components/room-item/index.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+
+const RoomItem = memo((props) => {
+  return (
+    <div>RoomItem </div>
+  )
+})
+
+RoomItem .propTypes = {}
+
+export default RoomItem 

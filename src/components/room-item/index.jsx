fa3ab9414c546470b603498fbfc966c47d5f156f
withLevi <zengyin.ssr@gmail.com>
2024-06-02 01:28:24 +0800

    Add room in the home page of the item package and overall layout display processing.

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -4,7 +4,18 @@ import { ItemWrapper } from "./style";
 
 const RoomItem = memo((props) => {
   const { itemData } = props;
-  return <ItemWrapper>{itemData.name}</ItemWrapper>;
+  return (
+    <ItemWrapper verifyColor={itemData?.verify_info.text_color || "#39576a"}>
+      <div className="inner">
+        <div className="cover">
+          <img src={itemData.picture_url} alt="" />
+        </div>
+        <div className="desc">{itemData.verify_info.messages.join(" . ")}</div>
+        <div className="name">{itemData.name}</div>
+        <div className="price">￥{itemData.price}/晚</div>
+      </div>
+    </ItemWrapper>
+  );
 });
 
 RoomItem.propTypes = {

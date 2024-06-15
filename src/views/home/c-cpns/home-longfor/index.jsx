d215a817cdbbdeaad405ae34ef4a07939a9881d2
withLevi <zengyin.ssr@gmail.com>
2024-06-16 00:20:37 +0800

    Add Component of would like to go item list.

--- src/views/home/c-cpns/home-longfor/index.jsx
+++ src/views/home/c-cpns/home-longfor/index.jsx
@@ -1,26 +1,28 @@
-import PropTypes from 'prop-types'
-import React, { memo } from 'react'
-import { LongforWrapper } from './style'
-import SectionHeader from '@/components/section-header'
+import PropTypes from "prop-types";
+import React, { memo } from "react";
+import { LongforWrapper } from "./style";
+import SectionHeader from "@/components/section-header";
+import LongforItem from "@/components/longfor-item";
+import ScrollView from "@/base-ui/scroll-view";
 
 const HomeLongfor = memo((props) => {
-    const { infoData } = props
+  const { infoData } = props;
   return (
     <LongforWrapper>
-        <SectionHeader title={infoData.title} subtitle={infoData.subtitle}/>
-        <div className='longfor-list'>
-            {
-                infoData.list.map(item => {
-                    return <div className='item'>{item.city}-{item.price}</div>
-                })
-            }
-        </div>
+      <SectionHeader title={infoData.title} subtitle={infoData.subtitle} />
+      <div className="longfor-list">
+        <ScrollView>
+          {infoData.list.map((item) => {
+            return <LongforItem itemData={item} key={item.city} />;
+          })}
+        </ScrollView>
+      </div>
     </LongforWrapper>
-  )
-})
+  );
+});
 
 HomeLongfor.propTypes = {
-    infoData: PropTypes.object
-}
+  infoData: PropTypes.object,
+};
 
-export default HomeLongfor
+export default HomeLongfor;
--- src/views/home/c-cpns/home-longfor/style.js
+++ src/views/home/c-cpns/home-longfor/style.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 
 export const LongforWrapper = styled.div`
-margin-top: 30px;
+    margin-top: 30px ;
+
+    .longfor-list {
+        display: flex;
+        margin: 0 -8px;
+    }
 
 `

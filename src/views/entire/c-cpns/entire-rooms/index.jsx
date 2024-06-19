ba72accd030c0ae3e50a28d72de081d5d851cdb1
withLevi <zengyin.ssr@gmail.com>
2024-06-19 23:45:56 +0800

    Add Get rooms's list data display process.

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,10 +1,24 @@
-import React, { memo } from 'react'
-import { RoomsWrapper } from './style'
+import React, { memo } from "react";
+import { RoomsWrapper } from "./style";
+import { useSelector } from "react-redux";
+import RoomItem from "@/components/room-item";
 
 const EntireRooms = memo(() => {
+  /** 从redux中获取roomList数据 */
+  const { roomList, totalCount } = useSelector((state) => ({
+    roomList: state.entire.roomList,
+    totalCount: state.entire.totalCount,
+  }));
   return (
-    <RoomsWrapper>EntireRooms</RoomsWrapper>
-  )
-})
+    <RoomsWrapper>
+      <h2 className="title">{totalCount}多处住所</h2>
+      <div className="list">
+        {roomList.map((item) => {
+          return <RoomItem itemData={item} itemWidth="20%" key={item.id} />;
+        })}
+      </div>
+    </RoomsWrapper>
+  );
+});
 
-export default EntireRooms
+export default EntireRooms;

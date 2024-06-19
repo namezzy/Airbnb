aba49e029c2075a8fe28324d431845a2b7477207
withLevi <zengyin.ssr@gmail.com>
2024-06-19 23:54:11 +0800

    Fix bug-Filter data show bugs.

--- src/views/entire/c-cpns/entire-filter/style.js
+++ src/views/entire/c-cpns/entire-filter/style.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const FilterWrapper = styled.div`
-  position: fixed;
+  /* position: fixed;
   top: 80px;
   left: 0;
   right: 0;
-  z-index: 9;
+  z-index: 9; */
+
   display: flex;
   align-items: center;
   height: 48px;

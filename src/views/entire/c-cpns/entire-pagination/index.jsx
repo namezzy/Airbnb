c3f46e074e96868c696c0a941d749fbc27cf78a9
withLevi <zengyin.ssr@gmail.com>
2024-06-20 23:15:58 +0800

    Add page number's dispplay.

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,10 +1,19 @@
 import React, { memo } from 'react'
+import { Pagination } from '@mui/material'
+
 import { PaginationWrapper } from './style'
 
 
 const EntirePagination = memo(() => {
   return (
-    <PaginationWrapper>EntirePagination</PaginationWrapper>
+    <PaginationWrapper>
+      <div className='info'>
+      <Pagination count={15}/>
+        <div className='desc'>
+          第x-yy个房源，共超过zzz个
+        </div>
+      </div>
+    </PaginationWrapper>
   )
 })
 
--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -3,4 +3,14 @@ import styled from "styled-components";
 
 export const PaginationWrapper = styled.div`
 
+display: flex;
+justify-content: center;
+
+.info {
+    display: flex;
+    justify-content: center;
+}
+
+
+
 `

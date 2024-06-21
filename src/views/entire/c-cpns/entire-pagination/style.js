a70611356830baa4b36678f9d2875a3dd74b4dea
withLevi <zengyin.ssr@gmail.com>
2024-06-21 17:30:36 +0800

    Add pagination of style.

--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
-
 export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 
-display: flex;
-justify-content: center;
-
-.info {
+  .info {
     display: flex;
+    flex-direction: column;
     justify-content: center;
-}
 
-
-
-`
+    .desc {
+        margin-top: 16px;
+    }
+  }
+`;

add67b0739a9489463b03669de5620f6dd305bc8
withLevi <zengyin.ssr@gmail.com>
2024-06-24 21:39:22 +0800

    Add Masked presentation and detailing of request data and add some style.

--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -5,25 +5,25 @@ import RoomItem from "@/components/room-item";
 
 const EntireRooms = memo(() => {
   /** 从redux中获取roomList数据 */
-  const { roomList, totalCount, isLoading } = useSelector((state) => ({
-    roomList: state.entire.roomList,
-    totalCount: state.entire.totalCount,
-    isLoading: state.entire.isLoading
-  }), shallowEqual);
-
+  const { roomList, totalCount, isLoading } = useSelector(
+    (state) => ({
+      roomList: state.entire.roomList,
+      totalCount: state.entire.totalCount,
+      isLoading: state.entire.isLoading,
+    }),
+    shallowEqual
+  );
 
   return (
     <RoomsWrapper>
       <h2 className="title">{totalCount}多处住所</h2>
       <div className="list">
-        {roomList.map((item,index) => {
-          return <RoomItem itemData={item} itemWidth="20%" key={index} />;
+        {roomList.map((item) => {
+          return <RoomItem itemData={item} itemWidth="20%" key={item._id} />;
         })}
       </div>
 
-    {
-      isLoading &&   <div className="cover"></div>
-    }
+      {isLoading && <div className="cover"></div>}
     </RoomsWrapper>
   );
 });

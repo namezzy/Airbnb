b66d6efd4f24a6690922e6b8e971f47900d1ba6a
withLevi <zengyin.ssr@gmail.com>
2024-06-27 22:04:44 +0800

    Add indicator's component wrapper-special event of left and right side .

--- src/base-ui/indicator/index.jsx
+++ src/base-ui/indicator/index.jsx
@@ -4,39 +4,42 @@ import React, { memo, useEffect, useRef } from "react";
 import { IndicatorWrapper } from "./style";
 
 const Indicator = memo((props) => {
-  const {selectIndex} = props
-  const contentRef = useRef()
+  const { selectIndex } = props;
+  const contentRef = useRef();
 
+  useEffect(() => {
+    // 获取selectIndex 对应的item
+    const selectItemEl = contentRef.current.children[selectIndex];
+    const itemLeft = selectItemEl.offsetLeft;
+    const itemWidth = selectItemEl.clientWidth;
 
-  useEffect( () => {
-      // 获取selectIndex 对应的item
-      const selectItemEl = contentRef.current.children[selectIndex]
-      const itemLeft = selectItemEl.offsetLeft
-      const itemWidth = selectItemEl.clientWidth
+    // content的宽度
+    const contentWidth = contentRef.current.clientWidth;
+    const contentScroll = contentRef.current.scrollWidth;
 
-      // content的宽度
-      const contentWidth = contentRef.current.clientWidth
+    // 获取selectIndex要滚动的距离
+    let distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5;
 
+    // 特殊情况处理
+    if (distance < 0) distance = 0; // 左边的特殊情况处理
+    const totalDistance = contentScroll - contentWidth;
+    if (distance > totalDistance) distance = totalDistance; // 右边的特殊情况处理
 
-      // 获取selectIndex要滚动的距离
+    // 改变位置即可
+    contentRef.current.style.transform = `translate(${-distance}px)`;
+  }, [selectIndex]);
 
-      const distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5
-
-      contentRef.current.style.transform = `translate(${-distance}px)`
-
-  },[selectIndex])
-
-  return <IndicatorWrapper>
-    <div className="i-content" ref={contentRef}>
-      {
-        props.children
-      }
-    </div>
-  </IndicatorWrapper>;
+  return (
+    <IndicatorWrapper>
+      <div className="i-content" ref={contentRef}>
+        {props.children}
+      </div>
+    </IndicatorWrapper>
+  );
 });
 
 Indicator.propTypes = {
-  selectIndex: PropTypes.number
+  selectIndex: PropTypes.number,
 };
 
 export default Indicator;

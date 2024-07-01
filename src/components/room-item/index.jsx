cb3dcd67983a440b3d149608c0c3dc4d9258133d
withLevi <zengyin.ssr@gmail.com>
2024-07-02 07:24:14 +0800

    Fix bugs- Determine if item room displays different images in different situations.

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -10,63 +10,79 @@ import classNames from "classnames";
 
 const RoomItem = memo((props) => {
   const { itemData, itemWidth = "25%" } = props;
-  const [selectIndex, setSelectIndex ] = useState(0)
+  const [selectIndex, setSelectIndex] = useState(0);
 
-  const sliderRef = useRef()
+  const sliderRef = useRef();
 
   /** 事件处理的逻辑 */
-  function controlClickHandle(isRight = true ) {
+  function controlClickHandle(isRight = true) {
     // 上一个面板/下一个面板
-      isRight ? sliderRef.current.next(): sliderRef.current.prev()
+    isRight ? sliderRef.current.next() : sliderRef.current.prev();
 
-      // 最新的索引
-      let newIndex = isRight ? selectIndex + 1 : selectIndex - 1
-      const length = itemData.picture_urls.length;
-      if(newIndex < 0  ) newIndex = length - 1
-      if(newIndex > length - 1 )  newIndex = 0
-      setSelectIndex(newIndex)
+    // 最新的索引
+    let newIndex = isRight ? selectIndex + 1 : selectIndex - 1;
+    const length = itemData.picture_urls.length;
+    if (newIndex < 0) newIndex = length - 1;
+    if (newIndex > length - 1) newIndex = 0;
+    setSelectIndex(newIndex);
   }
+
+  const pictureElement = (
+    <div className="cover">
+      <img src={itemData.picture_url} alt="" />
+    </div>
+  );
+
+  const sliderElement = (
+    <div className="slider">
+    <div className="control">
+      <div
+        className="btn left"
+        onClick={(e) => controlClickHandle(false)}
+      >
+        <IconArrowLeft width="30" height="30" />
+      </div>
+      <div
+        className="btn right"
+        onClick={(e) => controlClickHandle(true)}
+      >
+        <IconArrowRight width="30" height="30" />
+      </div>
+    </div>
+    <div className="indicator">
+      <Indicator selectIndex={selectIndex}>
+        {itemData?.picture_urls?.map((item, index) => {
+          return (
+            <div className="dot-item" key={item}>
+              <span
+                className={classNames("dot", {
+                  active: selectIndex === index,
+                })}
+              ></span>
+            </div>
+          );
+        })}
+      </Indicator>
+    </div>
+    <Carousel dots={false} ref={sliderRef}>
+      {itemData?.picture_urls?.map((item) => {
+        return (
+          <div className="cover" key={item}>
+            <img src={item} alt="" />
+          </div>
+        );
+      })}
+    </Carousel>
+  </div>
+  )
   return (
     <ItemWrapper
       verifyColor={itemData?.verify_info?.text_color || "#39576a"}
       itemWidth={itemWidth}
     >
       <div className="inner">
-        {/* <div className="cover">
-          <img src={itemData.picture_url} alt="" />
-        </div> */}
-        <div className="slider">
-          <div className="control">
-            <div className="btn left" onClick={e => controlClickHandle(false)}>
-              <IconArrowLeft width="30" height="30"/>
-            </div>
-            <div className="btn right" onClick={e => controlClickHandle(true)}>
-              <IconArrowRight width="30" height="30" />
-            </div>
-          </div>
-          <div className="indicator">
-            <Indicator selectIndex={selectIndex}>
-              {
-                itemData?.picture_urls?.map((item,index)=> {
-                    return (
-                      <div className="dot-item" key={item}>
-                          <span className={classNames("dot", {active: selectIndex === index})}></span>
-                      </div>
-                    )
-                })
-              }
-            </Indicator>
-          </div>
-          <Carousel dots={false} ref={sliderRef}>
-            {itemData?.picture_urls?.map((item) => {
-              return (
-                <div className="cover" key={item}>
-                  <img src={item} alt="" />
-                </div>
-              );
-            })}
-          </Carousel>
-        </div>
+        {!itemData.picture_urls ? pictureElement: sliderElement}
+      
         <div className="desc">{itemData.verify_info.messages.join(" . ")}</div>
         <div className="name">{itemData.name}</div>
         <div className="price">￥{itemData.price}/晚</div>

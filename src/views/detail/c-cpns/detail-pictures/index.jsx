ce5e5b9ca962c2c5c870a6fd2cca55e0941f7e32
withLevi <zengyin.ssr@gmail.com>
2024-07-05 22:19:54 +0800

    Delete test file and  improvement of detail page information.

--- src/views/detail/c-cpns/detail-pictures/index.jsx
+++ src/views/detail/c-cpns/detail-pictures/index.jsx
@@ -1,15 +1,34 @@
-import React, { memo } from 'react'
-import { PicturesWrapper } from './style'
-import { useSelector } from 'react-redux'
+import React, { memo } from "react";
+import { PicturesWrapper } from "./style";
+import { useSelector } from "react-redux";
 
 const DetailPictures = memo(() => {
-    /** redux获取数据*/
-    const {detailInfo} = useSelector((state) => ({
-        detailInfo: state.detail.detailInfo
-    }))
+  /** redux获取数据*/
+  const { detailInfo } = useSelector((state) => ({
+    detailInfo: state.detail.detailInfo,
+  }));
   return (
-    <PicturesWrapper>{detailInfo.name}</PicturesWrapper>
-  )
-})
+    <PicturesWrapper>
+      <div className="pictures">
+        <div className="left">
+          <div className="item">
+            <img src={detailInfo?.picture_urls?.[0]} alt="" />
+            <div className="cover"></div>
+          </div>
+        </div>
+        <div className="right">
+          {detailInfo?.picture_urls?.slice(1, 5).map((item) => {
+            return (
+              <div className="item" key={item}>
+                <img src={detailInfo?.picture_urls?.[0]} alt="" />
+                <div className="cover"></div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </PicturesWrapper>
+  );
+});
 
-export default DetailPictures
+export default DetailPictures;

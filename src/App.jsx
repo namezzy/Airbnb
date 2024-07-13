f845384484bafdb19ac906c50c902cc69b8200cd
withLevi <zengyin.ssr@gmail.com>
2024-07-13 19:23:44 +0800

    Add Back to page top when page switch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React, { memo } from 'react'
-import { useRoutes } from 'react-router-dom'
-import routes from './router'
-import AppHeader from './components/app-header'
-import AppFooter from './components/app-footer'
+import React, { memo } from "react";
+import {  useRoutes } from "react-router-dom";
+import routes from "./router";
+import AppHeader from "./components/app-header";
+import AppFooter from "./components/app-footer";
+import useScrollTop from "./hooks/useScrollTop";
 
 const App = memo(() => {
+  useScrollTop()
+
   return (
-    <div className='app'>
+    <div className="app">
       <AppHeader />
-      <div className='page'>
-        {useRoutes(routes)}
-      </div>
+      <div className="page">{useRoutes(routes)}</div>
       <AppFooter />
     </div>
-  )
-})
+  );
+});
 
-export default App
+export default App;
